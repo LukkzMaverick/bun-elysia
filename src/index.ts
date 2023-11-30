475ba2f91462a17b3e13ca917d2c4c698914ae08
Lucas Oliveira Santana da Cunha <lucas.dacunha@bairesdev.com>
2023-11-30 18:27:29 -0300

    add something

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { Elysia } from "elysia";
 import groupRouter from "elysia-group-router";
-import { swagger } from '@elysiajs/swagger'
+import { swagger } from "@elysiajs/swagger";
+import { get } from "js-runtime";
 
-export const app = new Elysia().use(swagger()).use((app) => groupRouter(app, 'routes')).get('/', () => new Date().toISOString()).listen(process.env.PORT as string || 3000);
+export const app = new Elysia()
+  .use(swagger())
+  .use((app) => groupRouter(app, "routes"))
+  .get("/", () => new Date().toISOString())
+  .listen((process.env.PORT as string) || 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
 
+console.log(`Running with ${get()}`);
--- src/routes/pets/index.ts
+++ src/routes/pets/index.ts
@@ -1,23 +1,96 @@
-import { t, type Elysia } from 'elysia';
-import { prisma } from '../../prisma';
+import { t, type Elysia } from "elysia";
+import { prisma } from "../../prisma";
 
 export default function petsRoutes(app: Elysia, prefix: string) {
-  return app.group(prefix, (app) => app.get('', () => 'hello pets').post('/', async ({set, body}) => {
-    set.status = 201
-    return await prisma.pet.create({data: body})
-  }, {
-    body: t.Object({
-      name: t.String({example: "Afonso"}),
-      age: t.Integer({example: 2}),
-      favoriteFood: t.Optional(t.String({example: "Chicken"}))
-    }),
-    response: {
-      201: t.Object({
-        id: t.Integer({example: 1}),
-        name: t.String({example: "Afonso"}),
-        age: t.Integer({example: 2}),
-        favoriteFood: t.Optional(t.String({example: "Chicken"}))
-      })
-  } as any
-}));
+  return app.group(prefix, (app) =>
+    app
+      .get(
+        ":id",
+        async ({ set, params: { id } }): Promise<any> => {
+          try {
+            const pet = await prisma.pet.findUnique({ where: { id } });
+            if (!pet) {
+              set.status = 404;
+              return responseException("Pet Not Found", 404);
+            }
+            return pet;
+          } catch (error) {
+            set.status = 500;
+            return responseException("Internal Server Error", 500);
+          }
+        },
+        {
+          params: t.Object({ id: t.Numeric() }),
+          response: {
+            200: postResponse,
+            404: HttpException("Pet Not Found", 404),
+            500: HttpException("Internal Server Error", 500),
+          },
+          detail: { tags: ["Pets"] },
+        }
+      )
+      .get(
+        "",
+        async ({ set }): Promise<any> => {
+          try {
+            return await prisma.pet.findMany();
+          } catch {
+            set.status = 500;
+            return responseException("Internal Server Error", 500);
+          }
+        },
+        {
+          response: {
+            200: t.Array(postResponse),
+            500: HttpException("Internal Server Error", 500),
+          },
+          detail: { tags: ["Pets"] },
+        }
+      )
+      .post(
+        "/",
+        async ({ set, body }): Promise<any> => {
+          try {
+            set.status = 201;
+            return await prisma.pet.create({ data: body });
+          } catch (error) {
+            set.status = 500;
+            return responseException("Internal Server Error", 500);
+          }
+        },
+        {
+          body: t.Object({
+            name: t.String({ example: "Afonso" }),
+            age: t.Integer({ example: 2 }),
+            favoriteFood: t.Optional(t.String({ example: "Chicken" })),
+          }),
+          response: {
+            201: postResponse,
+            500: HttpException("Internal Server Error", 500),
+          },
+          detail: { tags: ["Pets"] },
+        }
+      )
+  );
+}
+
+const postResponse = t.Object({
+  id: t.Integer({ example: 1 }),
+  name: t.String({ example: "Afonso" }),
+  age: t.Integer({ example: 2 }),
+  favoriteFood: t.String({ example: "Chicken" }),
+});
+
+const HttpException = (msg: string, status: number) =>
+  t.Object({
+    message: t.String({ example: msg }),
+    status: t.Number({ example: status }),
+  });
+
+const responseException = (message: string, status: number): HttpInterface => {
+  return { message, status };
+};
+interface HttpInterface {
+  message: string;
+  status: number;
 }
